c8263fda0cd9d4c3ec4b6c3b4c33987200aa7af4
Ignarus <kitoroli98@gmail.com>
2022-11-08 22:00:33 +0300

    save

--- Program.py
+++ Program.py
@@ -2,12 +2,12 @@
 str1 = input("str1 = ")
 str2 = input("str2 = ")
 
-def count_in_str(_str1, _str2):
-    count = 0
-    for i in _str1:
-        if i == _str2:
-            count+=1
-    return count
 
-print(count_in_str(str1, str2))
+start = 0
+end = len(str1)
+count = 0
+while str1.find(str2,start, end) != -1:
+    start = str1.find(str2, start, end) + len(str2)
+    count += 1
+print(count)
 
