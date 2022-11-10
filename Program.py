08ddc747911730fcabd0fd7268861b2774c57517
Ignarus <kitoroli98@gmail.com>
2022-11-10 13:19:20 +0300

    save

--- Program.py
+++ Program.py
@@ -2,3 +2,16 @@ colors = ["red", "green", "blue"]
 data = open("file.txt", "a") # a - дописать, r - считать, w - переписать
 data.writelines(colors)
 data.close()
+
+with open("file.txt", "w") as data:
+    data.write("line 1\n")
+    data.write("line 2\n") #автоматически закрывается
+
+
+path = "file.txt"
+data = open(path, "r")
+for line in data:
+    print(line)
+data.close()
+
+exit()
