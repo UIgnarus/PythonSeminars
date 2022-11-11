19ca744198b65df3cf8933d1776c42956fa0bc6b
Ignarus <kitoroli98@gmail.com>
2022-11-11 21:53:17 +0300

    save

--- Program.py
+++ Program.py
@@ -10,17 +10,17 @@ ax**2+ bx +c =0
 """
 
 def square_root(list):
-    d = list[1]**2 - (4*list[0] * list[2])
+    a,b,c = list[0], list[1], list[2]
+    d = b**2 - (4*a*c)
     print(d)
     if d < 0:
         return None
     elif d == 0:
-        return -(list[1]/(2*list[0]))
+        return -(b/(2*a))
     if d > 0:
-        x_1 = (-(list[1])+d**0.5)/(2*list[0])
-        x_2 = (-(list[1])-d**0.5)/(2*list[0])
+        x_1 = (-(b)+d**0.5)/(2*a)
+        x_2 = (-(b)-d**0.5)/(2*a)
         return [x_1, x_2]
-
 
 abc = [int(i) for i in input().split()]
 print(square_root(abc))
