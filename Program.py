f263a1fecf7243b7e956bef55e0ecadbc349c3cd
Ignarus <kitoroli98@gmail.com>
2022-11-17 20:59:33 +0300

    save

---
+++ Program.py
@@ -0,0 +1,13 @@
+string = list(input())
+coded_string = ''
+k = 1
+for i in range(len(string) - 1):
+    if string[i] == string[i + 1]:
+        k += 1
+        continue
+    else:
+        coded_string += str(k) + string[i]
+        k = 1
+coded_string += str(k) + string[i]
+print(coded_string)
+
